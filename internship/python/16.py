497c2248e581278b693ae9db1943a89d16368017
deekshitambig <ambigdeekshit@gmail.com>
2025-12-19 14:50:51 +0530

    updated

--- internship/python/16.py
+++ internship/python/16.py
@@ -1,6 +1,17 @@
-password=input("enter the password")
-for i in passwod:
-    if password(len)<8:
-        print("strong")
-    else:
-        print("weak")dfsd
+password=(input("enter the password"))
+digit= false
+letter= false
+for i in password:
+    if i<=1 or i<9:
+        digit=true
+    elif(i<='a' and i<='z') or  (i>='A' and i<='Z'):
+        letter=true
+        
+if password(len)>= 8:
+    if digit:
+        print("strong password")
+        if letter:
+            print("strong password")
+  
+else:
+    print("weak password")
